ffc427a6b19129a7b6d93cfc4b58b072e9873539
Bastian Onetto <bastian.onetto@usach.cl>
2020-09-18 01:27:28 -0300

    Se comienza con la interfaz

---
+++ git/GitRepo.java
@@ -0,0 +1,195 @@
+package git;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * Clase que representa el repositorio de Git completo, compuesto por el 
+ * el  nombre del autor del Proyecto, el nombre del mismo, la fecha de creacion
+ * y las zonas necesarias del Repositorio
+ * @author Bastian Onetto
+ */
+public class GitRepo {
+    private String autor;
+    private Date fecha;
+    private String nombreProyecto;	
+    private Zonas zonas;
+
+    //constructor    
+        
+    public GitRepo(){
+        this.autor = "";
+        this.fecha = null;
+        this.nombreProyecto = "";
+        this.zonas = new Zonas();
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNombreProyecto() {
+        return nombreProyecto;
+    }
+
+    public void setNombreProyecto(String nombreProyecto) {
+        this.nombreProyecto = nombreProyecto;
+    }
+
+    public Zonas getZonas() {
+        return zonas;
+    }
+
+    public void setZonas(Zonas zonas) {
+        this.zonas = zonas;
+    }
+
+    /**
+     * Inicializador del repositorio en Git, recibe como argumentos
+     * tanto el nombre del autor y el nombre del proyecto mismo.
+     * @param auth que es el nombre del autor del repositorio
+     * @param nombre que es el nombre del proyecto
+     */
+    public void gitInit(String auth, String nombre) {
+        this.autor = auth;
+        this.nombreProyecto = nombre;
+        //se inicializa la fecha como dato.
+        Date d= new Date();
+        this.fecha =d;
+        //se crea el objeto Zonas
+        Zonas z = new Zonas();
+        //Se inicializan de forma concreta los valores de Zona.
+        z.initZonas();
+        this.zonas = z;
+    }
+
+    /**
+     * Metodo gitCreateFile es el que nos permite crear archivos en el
+     * workspace.
+     */
+    public void gitCreateFile(){
+        this.zonas.agregarAWorkspace();
+    }
+
+    /**
+     * Metodo que le pide al usuario una lista de archivos para agregarlos
+     * al index, o con "all" que hace que se agreguen todos
+     */
+    public void gitAdd(){
+        /*Llamamos a la lista de archios*/
+        ArrayList<String> addInput;
+        addInput = tomaDatosAdd();
+        //verificamos si la entrada es o no "all"
+        if(addInput.size() >= 1){
+            if(addInput.size() == 1){
+                if(addInput.get(0).equals("all")){
+                    //si es all se agregan todos
+                    this.zonas.addAllToIndex();
+                }else{
+                    //si no, se agregan los de la lista
+                    this.zonas.addToIndex(addInput);
+                }
+            //si no, se agregan los de la lista
+            }else{
+                this.zonas.addToIndex(addInput);
+            }
+        }
+    }
+    /**
+    * Metodo necesario para poder obtener, del usuario, 
+    * @return una lista de Strings para usar en la funcion gitAdd. 
+    */
+    public ArrayList<String> tomaDatosAdd(){
+        // 
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Ingrese la lista de archivos a añadir o 'add' para añadir todos.\n(Los archivos van eparados por nada mas que una coma):\n");
+        String listaS = sc.nextLine();
+        while(listaS.length() == 0){
+            System.out.printf("La lista no puede quedar vacia, por favor reintente: ");
+            listaS = sc.nextLine();
+        }
+        String[] splitList;
+        splitList = listaS.split(",");
+        ArrayList<String> newList;
+        newList = new ArrayList<String>();
+        for (String splitList1 : splitList) {
+            if (!"".equals(splitList1)) {
+                newList.add(splitList1);
+            }
+        }
+        if(newList.isEmpty()){
+            System.out.println("La lista ha quedado vacia, por favor reintente.");
+            newList = tomaDatosAdd();
+        }
+        return newList;
+    }
+    /**
+     * gitCommit es el metodo con el cual podemos crear un commit, el cual 
+     * quedara guardado en el repositorio local, al terminar eso, se limpia 
+     * el index para esperar mas archivos para un commit.
+     */
+    public void gitCommit(){
+        this.zonas.doCommit();
+        this.zonas.index.clearFolder();
+    }
+
+    /**
+     * Este es el metodo con el cual llevamos los commits desde el local 
+     * repository al remote repository.
+     */
+    public void gitPush(){
+        this.zonas.push();
+    }
+    /**
+     * Metodo con el cual traemos al workspace todos los archivos del remote
+     * repository.
+     */
+    public void gitPull(){
+        this.zonas.pull();
+    }
+
+    /**
+     * El metodo gitStatus() nos entrega de manera visual los datos del
+     * estado actual en el cual se encuentra el repositorio del proyecto
+     * con la identificacion correspondiente.
+     */
+    public void gitStatus(){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        int work, ind, com;
+        /*llamamos a la cantidad de archivos existentes en tanto el index
+        como en el workspace, tambien llamamos la cantidad de commits en
+        el local repository, lo cual servira para hacer la verificacion 
+        del remote repository*/
+        work = this.zonas.workspace.list.largo();
+        ind = this.zonas.index.list.largo();
+        com = this.zonas.local.commitList.CL.size();
+        //llamamos al metodo verificador del estado del remote repository
+        boolean rep = this.zonas.remAlDia(com);
+        //luego mostramos todo por pantalla
+        System.out.printf("Nombre Proyecto: %s.\n"
+                + "Nombre Autor: %s.\n"
+                + "Fecha Creacion: %s\n"
+                + "####Estado Actual####\n"
+                + "Cantidad de Archivos:\n"
+                + "En Workspace: %d\n"
+                + "En Index: %d\n"
+                + "Cantidad de Commits en Local: %d\n"
+                + "Remote al dia: %b\n\n",
+                this.nombreProyecto, this.autor, formatter.format(this.fecha), work, ind, com, rep);
+    }
+        
+}
---
+++ git/Zonas.java
@@ -0,0 +1,148 @@
+package git;
+
+import java.util.ArrayList;
+
+/**
+ * Clase Zonas:
+ * Representa las 4 zonas de trabajo de un repositorio en git:
+ * Workspace: es donde se concentran los archivos que se esten manipulando
+ * Index: donde se preparan los archivos que se quieran llevar a un commit
+ * Local Repository: lugar en el computador donde se guarda la informacion
+ *                   sobre los commits hechos
+ * Remote Repository: espacio "en la nube" donde se encuentren los commits 
+ *                    respaldados.
+ * @author Bastian Onetto
+ */
+public class Zonas {
+    LocalFolder workspace;
+    LocalFolder index;
+    Repository local;
+    Repository remote;
+
+    //constructor
+    public Zonas() {
+    }
+
+    
+    
+    /**
+     * Inicializador de los datos de Zonas, con esto, cada uno de las zonas gana
+     * su identificador y sus valores por defecto respectivos.
+     */
+    public void initZonas(){
+        //generamos un nuevo objeto de cada uno
+        LocalFolder w = new LocalFolder();
+        w.LocalFolder("Workspace");
+        LocalFolder i = new LocalFolder();
+        i.LocalFolder("Index");
+        Repository l;
+        l= new Repository();
+        l.Repository("Local Repository");
+        Repository r;
+        r = new Repository();
+        r.Repository("Remote Repository");
+        //asignamos los valores
+        this.workspace = w;
+        this.index = i;
+        this.local = l;
+        this.remote = r; 
+    }
+    
+    /**
+     * Metodo el cual llama a addToFolder, lo cual agrega archivos al Workspace.
+     */
+    public void agregarAWorkspace(){
+        this.workspace.addToFolder();
+    }
+    
+    /**
+     * Metodo que pide una lista de nombres para obtener los archivos desde el 
+     * workspace y pasarlos uno por uno al index, para realizar las revisiones 
+     * necesarias
+     * @param SL, que corresponde a una lista de nombres de archivos. 
+     */
+    public void addToIndex(ArrayList<String> SL){
+        //Llamamos la lista de archivos
+        FileList listaWorks;
+        listaWorks = this.workspace.list.getFromFileList(SL);
+        File f;
+        /*colocamos uno por uno los archivos en el index, para realizar las
+        revisiones necesarias*/
+        for(int i = 0; i < listaWorks.largo(); i++){
+            f = listaWorks.FL.get(i);
+            this.index.list.addFiles(f);
+        }
+    }
+    /**
+     * Metodo el cual agrega todos los archivos del Workspace encontrados al 
+     * Index.
+     */
+    void addAllToIndex() {
+        //Llamamos la lista de archivos
+        FileList listaWorks;
+        listaWorks = this.workspace.list;
+        /*colocamos uno por uno los archivos en el index, para realizar las
+        revisiones necesarias*/
+        File f;
+        for(int i = 0; i < listaWorks.largo(); i++){
+            f = listaWorks.FL.get(i);
+            this.index.list.addFiles(f);
+        }   
+    }
+    
+    /**
+     * Funcion que permite crear un commit llamando a todos los archivos del 
+     * Index y luego pidiendo el comentario respectivo, para luego colocar este 
+     * commit a la cola de la lista de commits del repositorio Local
+     */
+    public void doCommit(){
+        FileList fL = this.index.list;
+        if(fL.FL.isEmpty()){
+            System.out.println("El index se encuentra vacio.\n"
+                    + "No se pudo acer un Commit");
+        }else{
+            Commit c;
+        c = new Commit();
+        c.makeCommit(fL);
+        this.local.repoCommit(c);
+        }
+        
+    }
+    /**
+     * Metodo que toma los commits hechos en el repositorio Local y los pasa
+     * al repositorio remoto, pasando tambien la lista de archivos.
+     */
+    public void push(){
+        //Primero llamamos la lista de commits y la pasamos al repositorio Remoto
+        CommitList c1;
+        c1 = this.local.commitList;
+        this.remote.commitList.pushCommits(c1);
+        //una vez pasados los commits, entregamos la lista de archivos
+        FileList f1;
+        f1 = this.local.repoFileList;
+        this.remote.repoFileList.mergeFiles(f1);
+    }
+    
+    /**
+     * Metodo el cual llama los archivos del Remote Repository y los pasa a la
+     * lista de archivos del workspace
+     */
+    public void pull(){
+        FileList listToWorkspace;
+        listToWorkspace = this.remote.repoFileList;
+        this.workspace.list.mergeFiles(listToWorkspace);
+    }
+    /**
+     * Metodo que comprueba si la cantidad de commits en el repositorio
+     * remoto coincide con la cantidad de commits del repositorio local.
+     * @param cant que representa la cantidad de Commits en el Local Repository
+     * @return Verdadero o Falso segun sea el caso
+     */
+    public boolean remAlDia(int cant){
+        boolean verif = false;
+        if(this.remote.commitList.CL.size() == cant){
+            verif = true;
+        }
+        return verif;
+    }
+}
